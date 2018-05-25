4eda5d1e1d3bcad82663ccbf51b048d44af61ec3
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-05-25 12:48:27 -0600

    size.html just needs some styling work. Functionality is gooood

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var sm = document.getElementById('sm');
 var med = document.getElementById('med');
 var lg = document.getElementById('lg');
 var xl = document.getElementById('xl');
+var price = document.getElementById('price');
+var size = document.getElementById('sizeDtls');
 
 var width = canvas.width;
 var height = canvas.height;
@@ -19,12 +21,20 @@ function displaySize(evt){
     ctx.strokeStyle = "rgb(0, 0, 0)";
     ctx.beginPath();
     if(evt.srcElement.id == "sm"){
+        price.innerHTML = "Total: $7.99";
+        size.innerHTML = 'SIZE: Small 8"';
         ctx.ellipse((width/2), (height/2), 25, 25, 0, 0, 2 * Math.PI);
     }else if(evt.srcElement.id == "med"){
+        price.innerHTML = "Total: $9.99";
+        size.innerHTML = 'SIZE: Medium 10"';
         ctx.ellipse((width/2), (height/2), 40, 40, 0, 0, 2 * Math.PI);
     }else if(evt.srcElement.id == "lg"){
+        price.innerHTML = "Total: $12.99";
+        size.innerHTML = 'SIZE: Large 12"';
         ctx.ellipse((width/2), (height/2), 50, 50, 0, 0, 2 * Math.PI);
     }else if(evt.srcElement.id == "xl"){
+        price.innerHTML = "Total: $14.99";
+        size.innerHTML = 'SIZE: X-Large 14"';
         ctx.ellipse((width/2), (height/2), 60, 60, 0, 0, 2 * Math.PI);
     }
     ctx.stroke(); 
