2a4f4cf8825406bda3ac92fea505b1968413b64c
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-05-29 13:22:46 -0600

    left, right, and full displays

--- script.js
+++ script.js
@@ -48,19 +48,192 @@ function nextPage(evt){
 
 var toppingArray = [];
 
-var bacon = document.getElementById('bacon');
-var pepperoni = document.getElementById('pepperoni');
+var topWidth = document.getElementsByClassName('topWidth');
+for(i = 0; i < topWidth.length; i++){
+    topWidth[i].addEventListener('click', changeImage);
+}
+var topAmount = document.getElementsByClassName('topAmount');
 
 bacon.addEventListener('click', addTopping);
 pepperoni.addEventListener('click', addTopping);
+chicken.addEventListener('click', addTopping);
+ham.addEventListener('click', addTopping);
+mushrooms.addEventListener('click', addTopping);
+olives.addEventListener('click', addTopping);
+onion.addEventListener('click', addTopping);
+pineapple.addEventListener('click', addTopping);
+spinach.addEventListener('click', addTopping);
+sausage.addEventListener('click', addTopping);
+
 bacon.addEventListener('click', removeTopping);
 pepperoni.addEventListener('click', removeTopping);
+chicken.addEventListener('click', removeTopping);
+ham.addEventListener('click', removeTopping);
+mushrooms.addEventListener('click', removeTopping);
+olives.addEventListener('click', removeTopping);
+onion.addEventListener('click', removeTopping);
+pineapple.addEventListener('click', removeTopping);
+spinach.addEventListener('click', removeTopping);
+sausage.addEventListener('click', removeTopping);
 
+function changeImage(evt){
+    canvas.style.background = "";
+    for(i = 0; i < topWidth.length; i++){
+        if(topWidth[0].value === "Left Half"){
+            left('bacon');
+        }else if(topWidth[0].value === "Right Half"){
+            right('bacon');
+        }else if(topWidth[0].value === "Full"){
+            full('bacon');
+        }
+        if(topWidth[1].value === "Left Half"){
+            left('pepperoni');
+        }else if(topWidth[1].value === "Right Half"){
+            right('pepperoni');
+        }
+        if(topWidth[2].value === "Left Half"){
+            left('pineapple');
+        }else if(topWidth[2].value === "Right Half"){
+            right('pineapple');
+        }
+        if(topWidth[3].value === "Left Half"){
+            left('mushrooms');
+        }else if(topWidth[3].value === "Right Half"){
+            right('mushrooms');
+        }
+        if(topWidth[4].value === "Left Half"){
+            left('sausage');
+        }else if(topWidth[4].value === "Right Half"){
+            right('sausage');
+        }
+        if(topWidth[5].value === "Left Half"){
+            left('olives');
+        }else if(topWidth[5].value === "Right Half"){
+            right('olives');
+        }
+        if(topWidth[6].value === "Left Half"){
+            left('ham');
+        }else if(topWidth[6].value === "Right Half"){
+            right('ham');
+        }
+        if(topWidth[7].value === "Left Half"){
+            left('spinach');
+        }else if(topWidth[7].value === "Right Half"){
+            right('spinach');
+        }
+        if(topWidth[8].value === "Left Half"){
+            left('chicken');
+        }else if(topWidth[8].value === "Right Half"){
+            right('chicken');
+        }
+        if(topWidth[9].value === "Left Half"){
+            left('onion');
+        }else if(topWidth[9].value === "Right Half"){
+            right('onion');
+        }
+    }
+}
+function full(top){
+    canvas.style.background = "";
+    for(var i =0; i < toppingArray.length; i++){
+        if(toppingArray[i].includes(top) === true){
+            toppingArray[i] = toppingArray[i].replace("left", "full");
+            toppingArray[i] = toppingArray[i].replace("right", "full");
+        }
+    }
+    var str = "";
+    for(var i =0; i < toppingArray.length; i++){
+        str += toppingArray[i]+", ";
+        if(i == toppingArray.length-1){
+            str = str.slice(0, -2);
+            canvas.style.background = str;
+        }
+    }
+    canvas.style.backgroundPosition = 'center';
+    canvas.style.backgroundRepeat = 'no-repeat';
+    canvas.style.backgroundSize = '100px';
+}
+function left(top){
+    canvas.style.background = "";
+    for(var i =0; i < toppingArray.length; i++){
+        if(toppingArray[i].includes(top) === true){
+            toppingArray[i] = toppingArray[i].replace("full", "left");
+            toppingArray[i] = toppingArray[i].replace("right", "left");
+        }
+    }
+    var str = "";
+    for(var i =0; i < toppingArray.length; i++){
+        str += toppingArray[i]+", ";
+        if(i == toppingArray.length-1){
+            str = str.slice(0, -2);
+            canvas.style.background = str;
+        }
+    }
+    canvas.style.backgroundPosition = 'center';
+    canvas.style.backgroundRepeat = 'no-repeat';
+    canvas.style.backgroundSize = '100px';
+}
+function right(top){
+    canvas.style.background = "";
+    for(var i =0; i < toppingArray.length; i++){
+        if(toppingArray[i].includes(top) === true){
+            toppingArray[i] = toppingArray[i].replace("full", "right");
+            toppingArray[i] = toppingArray[i].replace("left", "right");
+        }
+    }
+    var str = "";
+    for(var i =0; i < toppingArray.length; i++){
+        str += toppingArray[i]+", ";
+        if(i == toppingArray.length-1){
+            str = str.slice(0, -2);
+            canvas.style.background = str;
+        }
+    }
+    canvas.style.backgroundPosition = 'center';
+    canvas.style.backgroundRepeat = 'no-repeat';
+    canvas.style.backgroundSize = '100px';
+}
 function addTopping(evt){
     if(evt.srcElement.id === "bacon" && evt.target.checked === true){
+        topWidth[0].children[1].selected = true;
+        topAmount[0].children[1].selected = true;
         toppingArray.push("url(images/pizza/bacon-full.png)");
     } else if(evt.srcElement.id === "pepperoni" && evt.target.checked === true){
+        topWidth[1].children[1].selected = true;
+        topAmount[1].children[1].selected = true;
         toppingArray.push("url(images/pizza/pepperoni-full.png)");
+    }else if(evt.srcElement.id === "chicken" && evt.target.checked === true){
+        topWidth[8].children[1].selected = true;
+        topAmount[8].children[1].selected = true;
+        toppingArray.push("url(images/pizza/chicken-full.png)");
+    }else if(evt.srcElement.id === "ham" && evt.target.checked === true){
+        topWidth[6].children[1].selected = true;
+        topAmount[6].children[1].selected = true;
+        toppingArray.push("url(images/pizza/ham-full.png)");
+    }else if(evt.srcElement.id === "mushrooms" && evt.target.checked === true){
+        topWidth[3].children[1].selected = true;
+        topAmount[3].children[1].selected = true;
+        toppingArray.push("url(images/pizza/mushrooms-full.png)");
+    }else if(evt.srcElement.id === "olives" && evt.target.checked === true){
+        topWidth[5].children[1].selected = true;
+        topAmount[5].children[1].selected = true;
+        toppingArray.push("url(images/pizza/olives-full.png)");
+    }else if(evt.srcElement.id === "onion" && evt.target.checked === true){
+        topWidth[9].children[1].selected = true;
+        topAmount[9].children[1].selected = true;
+        toppingArray.push("url(images/pizza/onion-full.png)");
+    }else if(evt.srcElement.id === "pineapple" && evt.target.checked === true){
+        topWidth[2].children[1].selected = true;
+        topAmount[2].children[1].selected = true;
+        toppingArray.push("url(images/pizza/pineapple-full.png)");
+    }else if(evt.srcElement.id === "spinach" && evt.target.checked === true){
+        topWidth[7].children[1].selected = true;
+        topAmount[7].children[1].selected = true;
+        toppingArray.push("url(images/pizza/spinach-full.png)");
+    }else if(evt.srcElement.id === "sausage" && evt.target.checked === true){
+        topWidth[4].children[1].selected = true;
+        topAmount[4].children[1].selected = true;
+        toppingArray.push("url(images/pizza/sausage-full.png)");
     }
     var str = "";
     for(var i =0; i < toppingArray.length; i++){
@@ -75,16 +248,56 @@ function addTopping(evt){
     canvas.style.backgroundSize = '100px';
 }
 function removeTopping(evt){
-    var topping = "";
-
-    if(evt.srcElement.id === "bacon" && evt.target.checked === false){
-        topping = "url(images/pizza/bacon-full.png)";
-    } else if(evt.srcElement.id === "pepperoni" && evt.target.checked === false){
-        topping = "url(images/pizza/pepperoni-full.png)";
-    }
     for(var i = toppingArray.length-1; i >= 0 ; i--){
-        if(toppingArray[i] === topping){
-            toppingArray.splice(i, 1);
+        if(evt.srcElement.id === "bacon" && evt.target.checked === false){
+            topWidth[0].children[0].selected = true;
+            topAmount[0].children[0].selected = true;
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
+        } else if(evt.srcElement.id === "pepperoni" && evt.target.checked === false){
+            topWidth[1].children[0].selected = true;
+            topAmount[1].children[0].selected = true;
+            topping = "url(images/pizza/pepperoni-full.png)";
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
+        }else if(evt.srcElement.id === "chicken" && evt.target.checked === false){
+            topWidth[8].children[0].selected = true;
+            topAmount[8].children[0].selected = true;
+            topping = "url(images/pizza/chicken-full.png)";
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
+        }else if(evt.srcElement.id === "ham" && evt.target.checked === false){
+            topWidth[6].children[0].selected = true;
+            topAmount[6].children[0].selected = true;
+            topping = "url(images/pizza/ham-full.png)";
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
+        }else if(evt.srcElement.id === "mushrooms" && evt.target.checked === false){
+            topWidth[3].children[0].selected = true;
+            topAmount[3].children[0].selected = true;
+            topping = "url(images/pizza/mushrooms-full.png)";
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
+        }else if(evt.srcElement.id === "olives" && evt.target.checked === false){
+            topWidth[5].children[0].selected = true;
+            topAmount[5].children[0].selected = true;
+            topping = "url(images/pizza/olives-full.png)";
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
+        }else if(evt.srcElement.id === "onion" && evt.target.checked === false){
+            topWidth[9].children[0].selected = true;
+            topAmount[9].children[0].selected = true;
+            topping = "url(images/pizza/onion-full.png)";
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
+        }else if(evt.srcElement.id === "pineapple" && evt.target.checked === false){
+            topWidth[2].children[0].selected = true;
+            topAmount[2].children[0].selected = true;
+            topping = "url(images/pizza/pineapple-full.png)";
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
+        }else if(evt.srcElement.id === "spinach" && evt.target.checked === false){
+            topWidth[7].children[0].selected = true;
+            topAmount[7].children[0].selected = true;
+            topping = "url(images/pizza/spinach-full.png)";
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
+        }else if(evt.srcElement.id === "sausage" && evt.target.checked === false){
+            topWidth[4].children[0].selected = true;
+            topAmount[4].children[0].selected = true;
+            topping = "url(images/pizza/sausage-full.png)";
+            if(toppingArray[i].includes(evt.srcElement.id) === true) toppingArray.splice(i, 1);
         }
     }
     var str = "";
